914dbc2cb1dd05faf0ee213842784da7c12b001c
Thanh Nguyen <puffle.nnt@gmail.com>
2024-05-23 18:41:58 +0700

    push to dev1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log("developement from dev-1 branch");
+console.log("developement from dev-1 branch hello world");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
